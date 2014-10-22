ecad2dfe9eb09c8f82dfe36977624943b62ed3cc
matthijsfr <matthijsfr@gmail.com>
2014-10-22 22:40:18 +0200

    added some example code

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ dqbp
 ====
 
 dqbp website
+
+start the server from the git shell or bash,
+from root dir, type `DEBUG=dqbp ./bin/www`
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'dqbp' });
 });
+
+router.get('/wel', function(req, res) {
+  res.render('index', { title: 'wel' });
+});
+
+router.get('/:msg', function(req, res, next){
+    var theMsg = req.params.msg,
+        query = req.query;
+    console.log(theMsg, query);
+    res.json({theMsg: theMsg, query: query});
+});
+
+router.get('/niet', function(req, res) {
+  res.render('index', { title: 'niet' });
+});
+
+router.get('/plain/text', function(req, res){
+  res.send(200, 'plaintextfoyomama'); 
+})
 
 module.exports = router;
